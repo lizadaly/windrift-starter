cd37c484d52e440ab677806e4ee8060e77007f90
Liza Daly <lizadaly@gmail.com>
2016-10-24 22:35:09 -0400

    A house of things

--- chapters/chapter1.js
+++ chapters/chapter1.js
@@ -1,158 +1,56 @@
 const React = require('react')
-import { Map, List, RenderSection, FromInventory, NextChapter} from 'windrift'
+import { List, RenderSection } from 'windrift'
 
+const materials = ['A house of leaves', 'A house of plastic', 'A house of steel', 'A house of brick', 'A house of sand',
+   'A house of discarded clothing', 'A house of weeds', 'A house of wood', 'A house of roots',
+   'A house of paper', 'A house of broken dishes', 'A house of mud', 'A house of glass', 'A house of dust']
 
+const locations = ['on an island', 'in a deserted church', 'among other houses', 'by a river', 'in heavy jungle undergrowth',
+  'underwater', 'among small hills', 'in a place with both heavy rain and bright sun', 'on the sea', 'in a desert',
+  'in a cold, windy climate', 'in southern France', 'in Michigan', 'in dense woods']
+
+const lighting = ['using natural light', 'using all available lighting', 'using electricity', 'using candles']
+
+const inhabited = ['inhabited by all races of men represented wearing predominantly red clothing',
+  'inhabited by people who sleep almost all the time', 'inhabited by fishermen and families', 'inhabited by various birds and fish',
+  'inhabited by French- and German-speaking people', 'inhabited by lovers', 'inhabited by people who sleep very little',
+  'inhabited by collectors of all types', 'inhabited by people who enjoy eating together', 'inhabited by vegetarians',
+  'inhabited by friends', 'inhabited by little girls']
+
+var sections = []
 
 export default ({currentSection, inventory}) => {
-  const sections = [
-    <section>
-      <style>{`/* Some styles for this demo. Delete this when starting your own project. */
-        .game-text { margin: 1rem; padding: 1rem; font-size: 1rem; border-left: 1px dotted gray; font-family: Georgia; color: #485C5A }
-        pre { margin: .5rem; padding: .5rem; font-family: Consolas, Menlo, monospace; background: rgb(240,240,240)}
-      `}
-      </style>
-      <h2>Windrift Starter</h2>
-      <p>
-        Welcome to the starter application for <a href="https://github.com/lizadaly/windrift/">Windrift</a>, a framework
-        for writing interactive stories. The source code for this demo is available in
-        the <a href="https://github.com/lizadaly/windrift-starter">windrift-starter</a> repository.
-      </p>
-      <p>
-        This page will walk through some of the most common features of a Windrift story,
-        and can be used as a way to bootstrap your own story.
-      </p>
-      <h4>A List of two choices</h4>
-      <p>
-        You can choose an item, and both items will remain displayed:
-      </p>
-      <pre>{`<List expansions={[["dogs", "cats"], ["dogs", "cats"]}} tag="animal" conjunction="or"/>`}</pre>
 
-      <p className="game-text">
-        Do you prefer <List expansions={[["dogs", "cats"], ["dogs", "cats"]]} tag="animal" conjunction="or"/>?
-      </p>
-    </section>,
-    <section>
-      <h4>To display the user‘s response, just evaluate the inventory value in the markup directly:</h4>
-      <pre>{`<p>Hmm, {inventory.animal}, really?</p>`}</pre>
-      <p className="game-text">
-        Hmm, {inventory.animal}, really?
-      </p>
+  sections.push([<Stanza key={currentSection} />])
 
-      <h4>Doing transformations on responses</h4>
-      <p>
-        If you want to modify the user’s response somehow, like by pluralizing it,
-        you can use the {`<FromInventory>`} component to do so safely (even if the
-        value wasn’t yet set by the user):
-      </p>
-      <pre>{`One <FromInventory from={inventory.animal} onLoad={(from) => from.substring(0, from.length -1)} />, two{' '}
-{inventory.animal}. <FromInventory from={inventory.animal} onLoad={(from) => from.charAt(0).toUpperCase() + from.slice(1)} /> are great.
-`}</pre>
-      <p className="game-text">
-        One <FromInventory from={inventory.animal} onLoad={(from) => from.substring(0, from.length -1)} />, two{' '}
-        {inventory.animal}. <FromInventory from={inventory.animal} onLoad={(from) => from.charAt(0).toUpperCase() + from.slice(1)} /> are great.
-      </p>
-      <p>
-        If you expect to do a lot of these transformations, you could make a function or component
-        to wrap them:
-      </p>
-      <p className="game-text">
-        YOU MUST REALLY LOVE <AllCaps text={inventory.animal} /> HUH
-      </p>
-
-      <h4>Expanding in-place for effect:</h4>
-      <pre>{`<List expansions={["acceptable", "understandable", "admirable"]} tag="adj1" />`}</pre>
-      <p className="game-text">
-        I suppose that’s an <List expansions={["acceptable", "understandable", "admirable"]} tag="adj1" /> choice...
-      </p>
-    </section>,
-    <section>
-      <h4>Pick one, retain the answer</h4>
-      <pre>{`<List expansions={[["Vladimir", "Xiùlán", "Ikiaq"], "_last"]} tag="name1" conjunction="or" />`}</pre>
-      <p className="game-text">
-        What would you like to name your pet {inventory.animal ? inventory.animal.substring(0, inventory.animal.length -1): null}:{' '}
-        <List expansions={[["Vladimir", "Xiùlán", "Ikiaq"], "_last"]} tag="name1" conjunction="or" />
-      </p>
-    </section>,
-  <section>
-    <h4>Evaluating two different choices</h4>
-    <p className="game-text">If you had two {inventory.animal}, what would you name the other one:{' '}
-      <List expansions={[["Vladimir", "Xiùlán", "Ikiaq"], "_last"]} tag="name2" conjunction="or" />
-    </p>
-  </section>,
-  <section>
-
-    <p>
-      You can evaluate inventory values in ordinary JavaScript directly:
-    </p>
-
-    <pre>{`Looks like you’ll have two {inventory.animal} named
-    { inventory.name1 === inventory.name2 ? "the same" : "differently" }.`}</pre>
-
-    <p className="game-text">
-      Looks like you’ll have two {inventory.animal} named { inventory.name1 === inventory.name2 ? "the same" : "differently" }.
-    </p>
-
-    <h4>Using Maps</h4>
-
-    <p>
-      Maps can be used to simply return canned responses to choices:
-    </p>
-
-    <pre>{`<Map from={inventory.name2} to={{
-      vladimir: " that’s a fine Russian name",
-      xiùlán: " that’s a fine Chinese name",
-      ikiaq: " that’s a fine Inuit name"
-    }}/>`}</pre>
-
-    <p className="game-text">
-      {inventory.name2}:
-      <Map from={inventory.name2} to={{
-        vladimir: " that’s a fine Russian name",
-        xiùlán: " that’s a fine Chinese name",
-        ikiaq: " that’s a fine Inuit name"
-      }}/>.
-    </p>
-
-    <p>
-      But since Maps can return markup themselves, they can also be used to build up further choices:
-    </p>
-
-    <pre>{`<Map from={inventory.name2} to={{
-      vladimir: <p>Since you like Russian names, why not pick a second one from this set:{' '}
-      <List expansions={[["Alexei", "Darya", "Elena"], "_last"]} tag="name3" conjunction="or" /></p>,
-      xiùlán: <p>Since you like Chinese names, why not pick a second one from this set:{' '}
-      <List expansions={[["Li Jing", "Zhang Yan", "Wang Jie"], "_last"]} tag="name3" conjunction="or" /></p>,
-      ikiaq: <p>Since you like Inuit names, why not pick a second one from this set:{' '}
-      <List expansions={[["Naaqtuuq", "Pakak", "Toklo"], "_last"]} tag="name3" conjunction="or" /></p>,
-    }}/>`}</pre>
-
-    <div className="game-text">
-      <Map from={inventory.name2} to={{
-        vladimir: <p>Since you like Russian names, why not pick a second one from this set:{' '}
-          <List expansions={[["Alexei", "Darya", "Elena"], "_last"]} tag="name3" conjunction="or" /></p>,
-        xiùlán: <p>Since you like Chinese names, why not pick a second one from this set:{' '}
-          <List expansions={[["Li Jing", "Zhang Yan", "Wang Jie"], "_last"]} tag="name3" conjunction="or" /></p>,
-        ikiaq: <p>Since you like Inuit names, why not pick a second one from this set:{' '}
-          <List expansions={[["Naaqtuuq", "Pakak", "Toklo"], "_last"]} tag="name3" conjunction="or" /></p>,
-      }}/>
-    </div>
-
-  </section>,
-  <section>
-    <p>
-      For more advanced uses of Maps and Lists, see the <a href="https://lizadaly.github.io/windrift/examples/advanced/">Advanced demo</a> and <a href="https://github.com/lizadaly/windrift/blob/master/examples/advanced/chapters/chapter1.js">sample code</a>.
-    </p>
-    <p className="game-text">
-      Great, you can now begin your story with your two {inventory.animal}, {inventory.name1} and {inventory.name3}.
-    </p>
-
-    <NextChapter chapter={2}/>
-
-  </section>
-  ]
   return <RenderSection currentSection={currentSection} sections={sections} />
 }
 
-const AllCaps = ({text}) => (
-  <FromInventory from={text} onLoad={(text) => text.toUpperCase()} />
+const Stanza = () => {
+  var line = <div className="stanza">
+    <p>{ wrap(rand(materials)) }</p>
+    <p>{ wrap(rand(locations)) }</p>
+    <p>{ wrap(rand(lighting)) }</p>
+    <p>{ wrap(rand(inhabited)) }</p>
+  </div>
+  return line
+}
+
+const wrap = (line) => {
+  var words = line.split(' ')
+  var i = randFromLen(words)
+  var ret = words.map((word, index) => {
+    word += ' ' // Ensure our whitespace doesn't get collapsed
+    if (index === i)
+      return <List expansions={[word + ' ', word]} tag={word} key={word} />
+    return <span key={word}>{word}</span>
+  })
+  return ret
+}
+
+const rand = (items) => {
+  return items[randFromLen(items)]
+}
+const randFromLen = (items) => (
+  Math.floor(Math.random() * items.length)
 )
