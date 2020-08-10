88d513bec75e940cf280935b96ad02b0c21d191a
Paweł Świątkowski <katafrakt@vivaldi.net>
2020-08-10 02:41:17 +0200

    Upgrade dependencies to get it work on linux

--- webpack.config.js
+++ webpack.config.js
@@ -39,8 +39,8 @@ module.exports = [{
     filename: "story.js"
   },
   module: {
-    loaders: [{ test: /.js/, loaders: ['babel-loader?cacheDirectory']},
-              { test: /\.json$/, loader: 'json-loader' },
+    rules: [
+              { test: /\.js$/, loaders: ['babel-loader?cacheDirectory']},
               { test: /\.html$/, loader: 'html-loader'},
               { test: /\.hbs/, loader: 'handlebars-loader'},
               { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
